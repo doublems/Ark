afaf1f7ef2880686d015bf144a9633ee6ce9cc1d
jay <jb7959@naver.com>
2021-12-22 23:01:55 +0900

    add

---
+++ CodingTest/LeetCode/104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,24 @@
+/**
+ * 왼쪽 오른쪽 순으로 비교 후 올리면서 숫자 증가
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
