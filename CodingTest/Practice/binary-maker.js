aa22b9b90a8bc7853392a4d43120295873dddcd1
jay <jb7959@naver.com>
2022-01-16 17:39:26 +0900

    add

---
+++ CodingTest/Practice/binary-maker.js
@@ -0,0 +1,19 @@
+const bi = (number) => {
+    let quotient = number;
+    let reminder = [];
+
+    while (quotient >= 2){
+        console.log(quotient);
+        reminder.push(quotient % 2);
+        quotient = Math.floor(quotient / 2);
+    }
+    quotient = quotient + '';
+
+    while (reminder.length > 0) {
+        quotient = quotient + reminder.pop();
+    }
+
+    return quotient;
+}
+
+console.log(bi(5))
