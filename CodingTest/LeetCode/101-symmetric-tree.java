10a5109d28a7cd327f663a137654d9e13867ffb1
jay <jb7959@naver.com>
2022-01-25 16:49:17 +0900

    add

---
+++ CodingTest/LeetCode/101-symmetric-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root.left);
+        stack.push(root.right);
+
+        while(!stack.empty()){
+            TreeNode right = stack.pop();
+            TreeNode left = stack.pop();
+
+            if(right == null && left == null) {
+                continue;
+            }else if(right == null || left == null || right.val != left.val){
+                return false;
+            }
+
+            stack.push(left.left);
+            stack.push(right.right);
+            stack.push(left.right);
+            stack.push(right.left);
+        }
+
+        return true;
+    }
+}
