88047cc7e29e11727f94eca39962e208b7bad67b
jay <jb7959@naver.com>
2022-02-03 17:02:18 +0900

    add

---
+++ CodingTest/LeetCode/112-path-sum.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean result = false;
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        if(isLeaf(root) && root.val == targetSum) result = true;
+
+        run(root.left, targetSum - root.val);
+        run(root.right, targetSum - root.val);
+
+        return result;
+    }
+
+    private void run(TreeNode n, int targetSum){
+        if(n != null && result == false){
+            run(n.left, targetSum - n.val);
+            run(n.right, targetSum - n.val);
+            if(isLeaf(n) && targetSum - n.val == 0) result = true;
+        }
+    }
+
+    private boolean isLeaf(TreeNode n){
+        return n.left == null && n.right == null;
+    }
+}
